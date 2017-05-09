e70818912ef85993b9b9e013d4fee82c1bfe4b55
Alicemaina <alicemainachege@gmail.com>
2017-05-09 17:04:26 +0300

    Update Add links for project More infor Charged formatX

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+/*-------------------------------------------
+Typewriting init script
+---------------------------------------------*/
+
+function Typewriting() {
+
+
+$(document).ready(function(){
+	setTimeout( function(){
+		if($("#site-type").length) {
+        $(".typewrite span").typed({
+            strings: ["show case ", "projects "],
+            typeSpeed: 100,
+            backDelay: 500,
+            loop: false,
+            contentType: 'html', // or text
+            // defaults to false for infinite loop
+            loopCount: false,
+        });
+    }
+	}, 3000);
+});
+}
